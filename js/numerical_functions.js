729a3dc43fb119506a24ae39463915f7be6a068a
kawanao <bathbubbles3@yahoo.co.jp>
2015-11-25 12:55:36 +0900

    テスト前の練習問題

--- js/numerical_functions.js
+++ js/numerical_functions.js
@@ -21,7 +21,7 @@ function mod(a, b){
 
 function scale(a, min, max){
   var range = subtract(max, min);
-  
+
   a = Math.max(a, min);
   a = Math.min(a, max);
   a = subtract(a, min);
@@ -34,4 +34,20 @@ function sigmoid(value, gain){
   value = multiply(value, gain);
   var denominator = 1 + Math.exp(value);
   return divide(1, denominator);
+}
+
+function circleline(value){
+  return multiply(value,Math.PI);
+}
+
+function circlearea(value){
+  return multiply(multiply(value,value),Math.PI);
+}
+
+function largestcirclearea(a,b,c){
+  d = Math.max(a,b);
+  e = Math.max(b,c);
+  f = d*d*Math.PI;
+  g = e*e*Math.PI;
+  return Math.max(f,g);
 }
